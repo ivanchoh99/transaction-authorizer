380f658a81f6366b961fdc224009cdd9da43b468
Ivan H Carvajal A <ivanchoh99@gmail.com>
2026-01-28 21:22:34 -0500

    add transaction model domain

---
+++ src/main/java/org/example/domain/Transaction.java
@@ -0,0 +1,29 @@
+package org.example.domain;
+
+import org.example.models.TransactionDTO;
+
+import java.time.Instant;
+
+public class Transaction {
+    private String merchant;
+    private long amount;
+    private Instant time;
+
+    public Transaction(TransactionDTO transactionDTO) {
+        merchant = transactionDTO.merchant();
+        amount = transactionDTO.amount();
+        time = Instant.parse(transactionDTO.time());
+    }
+
+    public Instant getTime() {
+        return time;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+}
