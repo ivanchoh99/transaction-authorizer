0ece365d6f79c63eace01585500a20f09fe5ef14
Ivan H Carvajal A <ivanchoh99@gmail.com>
2026-01-28 21:21:56 -0500

    add Constants

---
+++ src/main/java/org/example/util/Constants.java
@@ -0,0 +1,7 @@
+package org.example.util;
+
+public final class Constants {
+
+    public static final Long LIMIT_TRANSACTION_IN_TIME = 3L;
+    public static final Long LIMIT_TIME_SECONDS = 120L;
+}
