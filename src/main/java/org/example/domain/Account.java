3e8e97ea2fca9cc38bf40c36db574c30c225e530
Ivan H Carvajal A <ivanchoh99@gmail.com>
2026-01-28 21:21:37 -0500

    update logic of account

--- src/main/java/org/example/domain/Account.java
+++ src/main/java/org/example/domain/Account.java
@@ -1,34 +1,69 @@
 package org.example.domain;
 
 import org.example.models.AccountDTO;
-import org.example.models.Transaction;
+import org.example.models.ResponseDTO;
+import org.example.util.Constants;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Account {
 
     private boolean activeCard;
     private long availableLimit;
-    private ConcurrentLinkedQueue<Transaction> transactions;
+    private final Deque<Transaction> transactions = new ArrayDeque<>();
 
     public Account(AccountDTO accountDTO) {
         activeCard = accountDTO.activeCard();
         availableLimit = accountDTO.availableLimit();
     }
 
-    public boolean isActiveCard() {
-        return activeCard;
-    }
-
-    public void updateAvailableLimit(Long transactionAmount) {
-        if (availableLimit > transactionAmount) {
-            availableLimit -= transactionAmount;
+    public synchronized ResponseDTO processTransaction(Transaction transactionToProcess) {
+        Instant timeBefore = Instant.now().minusSeconds(Constants.LIMIT_TIME_SECONDS);
+        this.cleanTransactions(timeBefore);
+        List<String> violations = validate(transactionToProcess, timeBefore);
+        if (violations.isEmpty()) {
+            transactions.add(transactionToProcess);
+            availableLimit -= transactionToProcess.getAmount();
         }
+        return new ResponseDTO(activeCard, availableLimit, violations);
     }
 
+    private List<String> validate(Transaction transactionToProcess, Instant timeBefore) {
+        List<String> violations = new ArrayList<>();
+        if (!activeCard) {
+            violations.add("card-not-active");
+        }
+        if (availableLimit < transactionToProcess.getAmount()) {
+            violations.add("insufficient-limit");
+        }
+        if (violations.isEmpty()) {
+            if (numberTransactionInLimitTime(timeBefore)) {
+                violations.add("high-frequency-small-interval");
+            }
+            if (repeatedTransactionInLimitTime(transactionToProcess, timeBefore)) {
+                violations.add("doubled-transaction");
+            }
+        }
+        return violations;
+    }
 
+    private boolean numberTransactionInLimitTime(Instant timeBefore) {
+        return transactions.size() > Constants.LIMIT_TRANSACTION_IN_TIME;
+    }
 
-    public void changeCardStatus(boolean activeCard) {
-        this.activeCard = activeCard;
+    private boolean repeatedTransactionInLimitTime(Transaction transactionToProcess, Instant timeBefore) {
+        return transactions.stream().anyMatch(transaction ->
+                transaction.getAmount() == transactionToProcess.getAmount() &&
+                        transaction.getMerchant().equals(transactionToProcess.getMerchant()));
+    }
+
+    private void cleanTransactions(Instant timeBefore) {
+        while (!transactions.isEmpty() && transactions.peekFirst().getTime().isBefore(timeBefore)) {
+            transactions.pollFirst();
+        }
     }
 }
