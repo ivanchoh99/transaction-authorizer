dad72231553d9e5af5c136cb3222fe15061babe7
Ivan H Carvajal A <ivanchoh99@gmail.com>
2026-01-29 19:25:01 -0500

    add constants of violations strings

--- src/main/java/org/example/util/Constants.java
+++ src/main/java/org/example/util/Constants.java
@@ -4,4 +4,10 @@ public final class Constants {
 
     public static final Long LIMIT_TRANSACTION_IN_TIME = 3L;
     public static final Long LIMIT_TIME_SECONDS = 120L;
+    public static final String ACCOUNT_INITIALIZED_MESSAGE = "account-already-initialized";
+    public static final String ACCOUNT_NOT_INITIALIZED_MESSAGE = "account-not-initialized";
+    public static final String CARD_NO_ACTIVE_MESSAGE = "card-not-active";
+    public static final String INSUFFICIENT_BALANCE_MESSAGE = "insufficient-limit";
+    public static final String TRANSACTION_LIMIT_IN_TIME_MESSAGE = "high-frequency-small-interval";
+    public static final String TRANSACTION_REPEATED = "doubled-transaction";
 }
