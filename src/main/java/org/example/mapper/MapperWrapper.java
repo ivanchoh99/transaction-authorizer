e34745e0886d501fc71ac0200c13d13ba4ab30f2
Ivan H Carvajal A <ivanchoh99@gmail.com>
2026-01-29 19:22:48 -0500

    Update MapperWrapper with methods

--- src/main/java/org/example/mapper/MapperWrapper.java
+++ src/main/java/org/example/mapper/MapperWrapper.java
@@ -1,9 +1,26 @@
 package org.example.mapper;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.example.models.AccountDTO;
 import org.example.models.TransactionDTO;
 
 public class MapperWrapper {
     private AccountDTO account;
     private TransactionDTO transaction;
+
+    public AccountDTO getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountDTO account) {
+        this.account = account;
+    }
+
+    public TransactionDTO getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(TransactionDTO transaction) {
+        this.transaction = transaction;
+    }
 }
